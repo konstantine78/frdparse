2d5a8a7ce9773b32c4a580e3d7b2d81d4de2f7b9
Konstantine <64042574+konstantine78@users.noreply.github.com>
2020-05-06 16:00:09 -0400

    Delete scratchpad.py

--- scratchpad.py
+++
@@ -1,8 +0,0 @@
-import re
-
-faultname_pattern = re.compile(r':(.*)\(')
-faultcode_pattern = re.compile(r'\b0x[0-9A-F]+\b')
-s = 'F: MyFaultName(MyFaultCode,0xaEAD1eEF,"This is my fault description.",0,3)'
-
-y = s.split('\"')[2].split(',')[2].split(')')[0].strip()
-print(y)
